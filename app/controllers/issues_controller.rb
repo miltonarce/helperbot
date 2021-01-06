a13fe659140ee42a7f04890e0d8cfc8bcb38eaf1
Milton <miltonhernan.arce@gmail.com>
2021-01-06 12:48:42 -0300

    Add send data to slack

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'json'
 
 class IssuesController < ApplicationController
 
@@ -9,6 +10,26 @@ class IssuesController < ApplicationController
     http.use_ssl = true
     res = http.get(url.request_uri)
     render :json => res.body
+ 
+    res_json = JSON.parse(res.body)
+   
+    # Token
+    token = "Bearer xoxb-1606047993505-1586911897958-csHC0mEH9y3gSPJsLrXFXOgz"
+     message = ""
+     
+    res_json.each {|n| 
+      text = "*#{n["title"]}*\n #{n["body"]} \n #{n["html_url"]} `To respond click on the link!` \n \n"
+      message += text
+    }
+
+
+    data = {channel: 'C01HU1E1AGZ', text: message, username: "HelperBot"}
+    urlToPost = URI.parse('https://slack.com/api/chat.postMessage')
+    httpToPost = Net::HTTP.new(urlToPost.host, urlToPost.port)
+    httpToPost.use_ssl = true
+    httpToPost.post(urlToPost, data.to_json, {"Content-Type" => "application/json", "Accept" => "application/json", "Authorization" => token,})
+    
+
     
     # @issues = Issue.all
     # render json: @issues
